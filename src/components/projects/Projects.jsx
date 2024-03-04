b63ecb4b1235f5747bb056be9922c36ffad08da3
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-05 02:01:57 +0300

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import "./projects.css"
-import amazonprojectimg from "../../assets/images/amazon-clone.jpg"
+import employeeManagement from "../../assets/images/employee-admin-page.png"
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -32,13 +32,13 @@ const Projects = () => {
       </div>
       <div className="card-of-projects smooth-projects">
         <div className="project-one">
-          <a href="https://clone-ddcf5.web.app" target='_blank' rel="noreferrer"><img src={amazonprojectimg} alt="projectimg" /></a>
+          <img src={employeeManagement} alt="projectimg" />
           <h5>Created an Employee Management System using Java/Spring Boot</h5>
           <h5>for the backend and React for the frontend. Features include</h5>
           <h5>CRUD operations for employees and tasks, admin privileges,</h5>
           <h5>and secure login with JWT authentication.</h5>
-          <h5>Github:https://github.com/Radwano01/employee-management-system-backend</h5>
-          <h5>Live demo:https://employee-management-system-admin.netlify.app</h5>
+          <h5>Github: https://github.com/Radwano01/employee-management-system-backend</h5>
+          <h5>Live demo: https://employee-management-system-admin.netlify.app</h5>
         </div>
       </div>
     </div>
