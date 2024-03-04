f37abffa47547088929ab510fa82e96ae1ea4050
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-04 23:39:00 +0300

    Update CertificatesData.js

--- src/data/CertificatesData.js
+++ src/data/CertificatesData.js
@@ -4,21 +4,56 @@ import certificateFive from "../assets/images/certificates/certificatefive.jpg"
 import certificateSix from "../assets/images/certificates/certificateSix.png"
 import certificateSeven from "../assets/images/certificates/certificateSeven.png"
 import certificateEight from "../assets/images/certificates/certificateeight.png"
+import certificateNine from "../assets/images/certificates/certificateten"
+import certificateTen from "../assets/images/certificates/certificatenine"
+import certificateeleven from "../assets/images/certificates/certificateeleven"
+import certificatetwelve from "../assets/images/certificates/certificatetwelve"
 
 
 const CertData = [
     {
-        id:1,
-        img:certificateOne,
-        date:'March 27, 2023',
-        TimeLine:"3 weeks",
-        Grade: "99.37%",
+        id:4,
+        img:certificateNine,
+        date:'January 18, 2024',
+        TimeLine:"11 hours",
+        Grade: "92.64%",
         verify:"Radwan Rahmoun Albakri's account is verified. Coursera certifies their",
-        successfull: "HTML, CSS, and Javascript for Web Developers",
-        link:"https://www.coursera.org/account/accomplishments/verify/2CH9BB8VWRQV"
+        successfull: "successful completion of Introduction to Java",
+        link:"https://www.coursera.org/account/accomplishments/verify/HKMMSYC6ZYHF"
     },
     {
-        id:4,
+        id:5,
+        img:certificateTen,
+        date:'March 4, 2024',
+        TimeLine:"17 hours",
+        Grade: "85.62%",
+        verify:"Radwan Rahmoun Albakri's account is verified. Coursera certifies their",
+        successfull: "successful completion of Introduction to Object-Oriented Programming with Java",
+        link:"https://www.coursera.org/account/accomplishments/verify/PZ6WTADRJNDQ"
+    },
+    {
+        id:6,
+        img:certificateeleven,
+        date:'January 18, 2024',
+        TimeLine:"14 hours",
+        Grade: "90.22%",
+        verify:"Radwan Rahmoun Albakri's account is verified. Coursera certifies their",
+        successfull: "successful completion of Java Programming: Arrays, Lists, and Structured Data",
+        link:"https://www.coursera.org/account/accomplishments/verify/NGARP8MX6YQU"
+    },
+    
+    {
+        id:7,
+        img:certificatetwelve,
+        date:'January 18, 2024',
+        TimeLine:"17 hours",
+        Grade: "92.85%",
+        verify:"Radwan Rahmoun Albakri's account is verified. Coursera certifies their",
+        successfull: "successful completion of Java Programming: Solving Problems with Software",
+        link:"https://www.coursera.org/account/accomplishments/verify/NCBV6EBWB2WL"
+    },
+    {
+        id:8,
         img:certificateFour,
         date:'August 1, 2023',
         TimeLine:"14 hours",
@@ -28,7 +63,17 @@ const CertData = [
         link:"https://www.coursera.org/account/accomplishments/verify/V3GA8QLUHT2B"
     },
     {
-        id:5,
+        id:9,
+        img:certificateOne,
+        date:'March 27, 2023',
+        TimeLine:"3 weeks",
+        Grade: "99.37%",
+        verify:"Radwan Rahmoun Albakri's account is verified. Coursera certifies their",
+        successfull: "HTML, CSS, and Javascript for Web Developers",
+        link:"https://www.coursera.org/account/accomplishments/verify/2CH9BB8VWRQV"
+    },
+    {
+        id:10,
         img:certificateFive,
         date:'July 24, 2023',
         TimeLine:"3 weeks and 2 days",
@@ -38,7 +83,7 @@ const CertData = [
         link:"https://course.zinotrustacademy.com/tutor-certificate/?cert_hash=096159803b005aaf"
     },
     {
-        id:6,
+        id:11,
         img:certificateSix,
         date:'August 6, 2023',
         TimeLine:"3 weeks and 3 days",
@@ -48,7 +93,7 @@ const CertData = [
         link:"https://course.zinotrustacademy.com/tutor-certificate/?cert_hash=c535a195d0d325b1"
     },
     {
-        id:7,
+        id:12,
         img:certificateSeven,
         date:'August 5, 2023',
         TimeLine:"16 hours (approximately)",
@@ -58,7 +103,7 @@ const CertData = [
         link:"https://www.coursera.org/account/accomplishments/verify/KML5SCYAV9G3"
     },
     {
-        id:8,
+        id:13,
         img:certificateEight,
         date:'August 28, 2023',
         TimeLine:"12 hours (approximately)",
@@ -67,6 +112,7 @@ const CertData = [
         successfull: "Developing Back-End Apps with Node.js and Express",
         link:"https://www.coursera.org/account/accomplishments/verify/U2QCXATXDWQJ"
     },
+
     
 
 ]
