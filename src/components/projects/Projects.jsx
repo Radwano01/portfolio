96372dba9b3bdc9b109d890eaf9eae8b37ea5f0a
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-05 00:58:17 +0300

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react'
 import "./projects.css"
 import amazonprojectimg from "../../assets/images/amazon-clone.jpg"
-import todosimg from "../../assets/images/todos.png"
-import calculatorimg from "../../assets/images/calculator.png"
-import shippingimg from "../../assets/images/shippingimg.png"
-import furnitureimg from "../../assets/images/furnitureimg.png"
-import petcare from "../../assets/images/petcareimg.png"
-import loginimg from "../../assets/images/loginimg.png"
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -48,55 +42,11 @@ const Projects = () => {
           <h5 style={{color: "red"}}>(Not: paymant issues isn't work because hosting is free)</h5>
         </div>
         <div className="project-two">
-          <a href="https://radwan-todos.netlify.app" target='_blank' rel="noreferrer"><img src={todosimg} alt="projectimg" /></a>
-          <h5>1- Todo-List Website Full-Stack</h5><br />
-          <h5>2- You can Add Todos</h5><br />
-          <h5>3- You can Check it To make it Done</h5><br />
-          <h5>4- You can Remove the Todos</h5><br />
-          <h5>5- builded by Redux Toolkit</h5><br />
-          <h5>6- You can check The Project code on GitHub <a href="https://github.com/Radwano01/Todos" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5>
-          <br /><br /><br />
           
         </div>
         <div className="project-three">
-          <a href="https://radwan-calculator.netlify.app" target='_blank' rel="noreferrer"><img src={calculatorimg} alt="projectimg" /></a>
-          <h5>1- Simple Calculator</h5><br />
-          <h5>2- You can check The Project code on GitHub <a href="https://github.com/Radwano01/calculator" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5>
-          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        </div>
-      </div>
-      <div className="card-of-projects smooth-projects">
-        <div className="project-four">
-          <a href="https://radwan-shipping-website.netlify.app" target='_blank' rel='noreferrer'><img src={shippingimg} alt="" /></a>
-          <h5>1- Shipping Landing Page</h5><br />
-          <h5>2- Responsive for all pages</h5><br />
-          <h5>3- You can check The Project code on GitHub <a href="https://github.com/Radwano01/shipping-frontend" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5>
-        </div>
-        <div className="project-five">
-          <a href="https://radwan-furniture-website.netlify.app" target='_blank' rel='noreferrer'><img src={furnitureimg} alt="" /></a>
-            <h5>1- Furniture Landing Page</h5><br />
-            <h5>2- Responsive for all pages</h5><br />
-            <h5>3- You can check The Project code on GitHub <a href="https://github.com/Radwano01/furniture-frontend" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5>
-        </div>
-        <div className="project-six">
-          <a href="https://radwan-petcare-website.netlify.app/" target='_blank' rel='noreferrer'><img src={petcare} alt="" /></a>
-            <h5>1- Pet Care Landing Page Design</h5><br />
-            <h5>2- Responsive for all pages</h5><br />
-            <h5>3- You can check The Project code on GitHub <a href="https://github.com/Radwano01/petcare-frontend" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5>
-        </div>
-      </div>
-      <div className="card-of-projects smooth-projects">
-        {/* <div className="project-seven">
-          <a href="#/" target='_blank' rel='noreferrer'><img src={loginimg} alt="" /></a>
-          <h5>1- Login /Register Page</h5><br />
-          <h5>2- You can Register</h5><br />
-          <h5>3- Verify the account by send a link</h5><br />
-          <h5>4- The password you regisered will be hashed in database</h5><br />
-          <h5>5- You can Reset Password</h5>
-          <h5>6- You can check The Project code on GitHub <a href="https://github.com/Radwano01/login-Register" style={{color: " blue", borderBottom: "1px solid blue"}} target='_blank' rel="noreferrer">Click</a></h5><br />
-          <h5 style={{color: "red"}}>(No Domain available for this project)</h5>
-        </div> */}
 
+        </div>
       </div>
     </div>
   )
