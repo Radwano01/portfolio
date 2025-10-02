84ae2dcb87d02d00f731d7c04b85e1eb136c3f84
Radwan <www.radwaniq@gmail.com>
2025-10-02 16:32:14 +0300

    build: output Vite build to build/ to match deploy settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Align Vite's output with your deploy setting expecting 'build/'
+    outDir: 'build',
+    emptyOutDir: true,
+  },
 })
